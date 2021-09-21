3687269a24445ca019d590789ab0ce190b97f9a7
Fred Saraiva <famsaraiva@gmail.com>
2021-09-21 12:06:33 -0300

    Config.ini com secao para site

--- src/main.py
+++ src/main.py
@@ -9,17 +9,16 @@ cfg.read("conf/config.ini")
 
 my_token = cfg.get('section1', 'token')
 my_chat_id = cfg.get('section2', 'chat_id')
-
+my_site = cfg.get('section3', 'site')
 
 if __name__ == '__main__':
 
-    site = "www.ufpb.br"
-    r = checkSite.check("http://" + site)
+    r = checkSite.check("http://" + my_site)
     print(r)
 
     while 1:
         time.sleep(120)
         if r == 1:
-            e.send('Tudo normal com ' + site, my_chat_id, my_token)
+            e.send('Tudo normal com ' + my_site, my_chat_id, my_token)
         else:
-            e.send('Problemas com ' + site, my_chat_id, my_token)
+            e.send('Problemas com ' + my_site, my_chat_id, my_token)
